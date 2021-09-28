1b08512cfd3f8b131950966c67ac097d3e54fc53
GeoffreyCoulaud <geoffrey.coulaud@gmail.com>
2021-09-28 11:39:35 +0200

    Start of the UI work

---
+++ UI/gtkUtils.js
@@ -0,0 +1,16 @@
+/**
+ * Assign an event handler to a GTK widget.
+ * Useful when you only need to assign an event on a widget. 
+ * @param {Gtk.Builder} builder - Builder to use
+ * @param {String} id - ID of the widget
+ * @param {String} event - Event's name
+ * @param {Function} handlers - Handler to assign
+ */
+function widgetHandleEvent(builder, id, event, handler){
+	const widget = builder.getObject(id);
+	widget.on(event, handler);
+}
+
+module.exports = {
+	widgetHandleEvent,
+};
--- main.js
+++ main.js
@@ -1,26 +1,61 @@
-// App entry point file.
-// The library is scanned, the UI is started and app management happens.
+const gi = require("node-gtk");
+const Gtk = gi.require("Gtk", "4.0");
+const GLib = gi.require("GLib", "2.0");
+const { widgetHandleEvent } = require("./UI/gtkUtils.js");
 
-const { app, BrowserWindow } = require("electron");
+let GtkLoop = undefined;
+let GtkApp = undefined;
 
-function createWindow(){
-	const options = {
-		width: 800,
-		height: 600,
-		autoHideMenuBar: true,
-	};
-	const mainWindow = new BrowserWindow(options);
-	mainWindow.loadFile("frontend/index.html");
-	mainWindow.webContents.openDevTools();
-	mainWindow.maximize();
+/**
+ * Event handler for the game life cycle buttons
+ */
+function onStartButtonClicked(){
+	console.log("Start button clicked");
+}
+function onStopButtonClicked(){
+	console.log("Stop button clicked");
+}
+function onKillButtonClicked(){
+	console.log("Kill button clicked");
 }
 
-// Start the UI
-app.whenReady().then(()=>{
-	createWindow();
-});
+/**
+ * Handle application close request
+ */
+function onCloseRequest(){
 
-// Close the process when all windows are closed
-app.on("window-all-closed", ()=>{
-	app.quit();
-});
+	GtkLoop.quit();
+	GtkApp.quit();
+	return false;
+
+}
+
+/**
+ * Handle application activation
+ */
+function onActivate(){
+
+	// Load UI definition from file
+	const builder = new Gtk.Builder.newFromFile("UI/brag-launcher.cmb.ui");
+
+	widgetHandleEvent(builder, "startGameButton", "clicked", onStartButtonClicked);
+	widgetHandleEvent(builder, "stopGameButton", "clicked", onStopButtonClicked);
+	widgetHandleEvent(builder, "killGameButton", "clicked", onKillButtonClicked);
+
+	const mainWindow = builder.getObject("mainWindow");
+	mainWindow.on("close-request", onCloseRequest);
+	mainWindow.show();
+	mainWindow.present();
+
+	// Start app
+	gi.startLoop();
+	GtkLoop.run();
+
+}
+
+// Start the app
+GtkLoop = GLib.MainLoop.new(null, false);
+GtkApp = new Gtk.Application("brag-launcher", 0);
+GtkApp.on("activate", onActivate);
+const status = GtkApp.run([]);
+console.log("Exiting with status :", status);
