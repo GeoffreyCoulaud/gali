df71c9a9b50c216ed48efbcb0d8f2845a1f2ee7e
GeoffreyCoulaud <geoffrey.coulaud@gmail.com>
2022-05-27 01:58:44 +0200

    🚧 Fixed union typing

--- gali/games/emulation_game.py
+++ gali/games/emulation_game.py
@@ -5,7 +5,7 @@ from gali.games.game import Game
 class EmulationGame(Game):
 	"""A class representing emulation games"""
 
-	game_path: Union(str, Path) = None
+	game_path: Union[str, Path ] = None
 
 	def __init__(self, name, game_path) -> None:
 		super().__init__(name)
--- gali/sources/game_dir.py
+++ gali/sources/game_dir.py
@@ -4,7 +4,7 @@ from typing import Union
 
 class GameDir():
 
-	path: Union(str, Path) = None
+	path: Union[str, Path] = None
 	depth: int = 0
 
 	def __init__(self, path, depth) -> None:
