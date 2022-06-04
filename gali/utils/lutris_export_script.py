ba8f68ec4c14d03fbc3254c2af2068c8a4201750
GeoffreyCoulaud <geoffrey.coulaud@gmail.com>
2022-06-04 14:36:19 +0200

    ✨ Implemented lutris_export_script

--- gali/utils/lutris_export_script.py
+++ gali/utils/lutris_export_script.py
@@ -1,3 +1,14 @@
+from subprocess import run
+
+from gali.utils.sandbox import is_flatpak
 
 def lutris_export_script(game_slug: str, script_path: str) -> None:
-	pass
+
+	# Build command
+	args = []
+	if is_flatpak():
+		args.extend(["flatpak-spawn", "--host"])
+	args.extend(["lutris", game_slug, "--output-script", script_path])
+
+	# Run command (can raise an error)
+	run(args, check=True)
