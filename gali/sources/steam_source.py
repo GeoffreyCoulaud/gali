b70e92102f442019b6365b11a2c2ed3715256378
GeoffreyCoulaud <geoffrey.coulaud@gmail.com>
2022-07-12 17:04:14 +0200

    🎨 Refactored steam_source.get_games
    Split into multiple steps:
    - get_manifest_paths
    - read_manifest
    - get_games

--- gali/sources/steam_source.py
+++ gali/sources/steam_source.py
@@ -7,6 +7,14 @@ from gali.games.steam_game import SteamGame
 from gali.utils.locations import HOME
 
 
+class InvalidManifestException(Exception):
+    pass
+
+
+class NonGameManifestException(Exception):
+    pass
+
+
 def fullmatch_any(value: str, patterns: tuple[re.Pattern]) -> bool:
     """Test if a string matches any regex in a tuple"""
     for pattern in patterns:
@@ -41,7 +49,7 @@ class SteamSource(Source):
         else:
             return config
 
-    def get_game_dirs(self, config: dict) -> tuple[str]:
+    def get_dir_paths(self, config: dict) -> tuple[str]:
         paths = []
         library_folders = config["libraryfolders"]
         if library_folders is None:
@@ -54,10 +62,11 @@ class SteamSource(Source):
             paths.append(path)
         return tuple(paths)
 
-    def get_games(self, game_dirs: tuple[str]) -> tuple[SteamGame]:
+    def get_manifest_paths(self, game_dirs: list[str]) -> list[str]:
+
+        mainfest_paths = []
 
         # Get all manifest paths
-        manifests = []
         for game_dir in game_dirs:
 
             # Test if dir is scannable
@@ -73,52 +82,71 @@ class SteamSource(Source):
                     continue
                 if not dirent.name.endswith(".acf"):
                     continue
-                manifests.append(dirent.path)
+                mainfest_paths.append(dirent.path)
+
+        return mainfest_paths
+
+    def read_manifest(self, manifest_path: str) -> SteamGame:
+
+        # Get data
+        try:
+            with open(manifest_path, "r", encoding="utf-8-sig") as file:
+                data = vdf.load(file)
+        except Exception as err:
+            raise err
+
+        # Get installation state
+        app_state = data.get("AppState", None)
+        if app_state is None:
+            raise InvalidManifestException("Missing AppState")
+        state_flags = int(app_state.get("StateFlags", 0))
+        is_installed = state_flags & self.installed_mask
+
+        # Get appid
+        app_id = app_state.get("appid", None)
+        if app_id is None:
+            raise InvalidManifestException("Missing appid")
+
+        # Get name
+        name = app_state.get("name", None)
+        if name is None:
+            raise InvalidManifestException("Missing name")
+
+        # Skip known non-games
+        if app_id in self.ignored_appids:
+            raise NonGameManifestException("AppId in ignore list")
+        if fullmatch_any(name, self.ignored_name_patterns):
+            raise NonGameManifestException("Name matches an ignore pattern")
+
+        # Build game
+        game = self.game_class(
+            name=name,
+            app_id=app_id,
+            is_installed=is_installed
+        )
+        return game
+
+    def get_games(self, manifest_paths: tuple[str]) -> tuple[SteamGame]:
 
         games = []
 
-        # Read manifests
-        for manifest in manifests:
-
-            # Get data
-            try:
-                with open(manifest, "r", encoding="utf-8-sig") as file:
-                    data = vdf.load(file)
-            except Exception as err:
-                raise err
-
-            # Get interesting info
-            app_state = data.get("AppState", None)
-            if app_state is None:
-                continue
-            state_flags = int(app_state.get("StateFlags", 0))
-            app_id = app_state.get("appid", None)
-            name = app_state.get("name", None)
-            is_installed = state_flags & self.installed_mask
-            if (app_id is None) or (name is None):
-                continue
-
-            # Skip known non-games
-            if app_id in self.ignored_appids:
-                continue
-            if fullmatch_any(name, self.ignored_name_patterns):
-                continue
+        # Read all manifests
+        for manifest_path in manifest_paths:
 
             # Build game
-            game = self.game_class(
-                name=name,
-                app_id=app_id,
-                is_installed=is_installed
-            )
+            try:
+                game = self.read_manifest(manifest_path)
+            except Exception:
+                continue
 
             # If an existing game has the same app id,
             # if the new is installed and not the old,
             # remove the old.
             for (i, old) in enumerate(games):
                 if (
-                    (old.app_id == game.app_id) and
-                    (not old.is_insalled) and
-                    game.is_installed
+                    old.app_id == game.app_id
+                    and not old.is_installed
+                    and game.is_installed
                 ):
                     del games[i]
 
@@ -128,8 +156,9 @@ class SteamSource(Source):
 
     def scan(self) -> tuple[SteamGame]:
         config = self.get_library_config()
-        game_dirs = self.get_game_dirs(config)
-        games = self.get_games(game_dirs)
+        game_dir_paths = self.get_dir_paths(config)
+        manifest_paths = self.get_manifest_paths(game_dir_paths)
+        games = self.get_games(manifest_paths)
         return games
 
 
