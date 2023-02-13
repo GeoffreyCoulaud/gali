f7e3684b42f00bed4091a5eec29ef938e8cde333
GeoffreyCoulaud <geoffrey.coulaud@gmail.com>
2023-02-13 17:07:05 +0100

    📝 Better doc for StartupChain class

--- gali/sources/startup_chain.py
+++ gali/sources/startup_chain.py
@@ -5,7 +5,20 @@ from gali.sources.game import Game
 class StartupChain(ABC):
     """Class representing a startup chain for a startable object.
     
-    The order of execution is prepare > start > cleanup"""
+    A startup chain represents a broad method of starting a game.
+    For example, with a ShellCommandStartupChain, if you would use a different command altogether, 
+    prefer using multiple StartupChain classes.
+
+    The goal is to present multiple launch methods to the end user, 
+    think of the Steam popup asking if you want to launch with DirectX or Vulkan.
+    
+    Don't use a different startup chain for small options change like fullscreen on/off.
+    In that case, specify launch options for the user that will be passed to the StartupChain.
+    
+    The order of execution is 
+    1. prepare, 
+    2. start
+    3. (game terminates) cleanup"""
 
     name: str
 
